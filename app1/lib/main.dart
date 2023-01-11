5708d4e62eeaeade0414568250118601c5112f32
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2023-01-12 00:00:37 +0700

    1/12/2023

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,41 +1,45 @@
-import 'package:app1/widgets/Footer.dart';
+import 'package:app1/widgets/TitleBar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // ignore: prefer_const_constructors
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My App",
       home: Scaffold(
         appBar: AppBar(
-          title: const Center(
-            child: Text("hello Flutter"),
-          ),
+          title: const Text("AppBar"),
         ),
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
               Text(
-                "123",
-                style: TextStyle(backgroundColor: Colors.blue),
-              ),
-              Text("123"),
-              Text("123"),
-              Footer(),
+                '$_counter',
+              )
             ],
           ),
         ),
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+        ),
       ),
     );
   }
---
+++ app1/lib/widgets/TitleBar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TitleBar extends StatelessWidget {
+  const TitleBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("LOL"),
+      ),
+    );
+  }
+}
