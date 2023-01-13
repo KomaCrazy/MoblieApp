7b21fb5ce46165a58a432fdf3371f185db870182
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2023-01-13 16:39:17 +0700

    1

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -16,8 +16,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(canvasColor: Colors.blue[100]),
-      home: PageMain(),
+      theme: ThemeData(canvasColor: Colors.blue[400]),
+      home: const PageMain(),
       debugShowCheckedModeBanner: false,
     );
   }
--- app1/lib/widgets/Componets.dart
+++ app1/lib/widgets/Componets.dart
@@ -12,22 +12,23 @@ class _PageMainState extends State<PageMain> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: Image.asset(
-            "assets/images/flutter.png",
-            width: 10,
-            height: 10,
-          ),
-          title: Text("Flutter")),
+        leading:
+            Image.asset("assets/images/flutter.png", width: 10, height: 10),
+        title: Text("Flutter"),
+      ),
       body: Column(
         children: const [
           Padding(
             padding: EdgeInsets.all(40),
             child: Center(
               child: Padding(
-                padding: EdgeInsets.all(10),
+                padding: EdgeInsets.all(0),
                 child: Text(
                   "Build apps for any Screen",
-                  style: TextStyle(fontSize: 24,fontWeight:),
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white),
                 ),
               ),
             ),
